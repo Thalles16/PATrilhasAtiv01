0cd5a66bc929a7fc9bfd35f348f4dc17b86f8536
Thalles <168739769+Thalles16@users.noreply.github.com>
2025-01-28 10:21:22 -0300

    questao 6.cs

---
+++ questao 6.cs
@@ -0,0 +1,21 @@
+
+using System;
+	
+	class program 
+{
+	static void Main ()
+	{ 
+		Console.WriteLine("escolha um numero decimal");
+		decimal numero = decimal.Parse(Console.ReadLine());
+		Console.Write("o numero digitado  é " +numero);
+	Console.Write(Console.ReadLine());
+	
+	
+	
+	
+	Console.WriteLine("aperte qualquer tecla para fechar");
+		Console.ReadKey();
+    
+		
+	} 
+}
