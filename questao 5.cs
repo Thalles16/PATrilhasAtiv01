337f48ebcd1b5ec1236da60c77c11a6ca5ad6c3a
Thalles <168739769+Thalles16@users.noreply.github.com>
2025-01-28 09:29:11 -0300

    questao 5.cs

---
+++ questao 5.cs
@@ -0,0 +1,20 @@
+using System;
+	
+	class program 
+{
+	static void Main ()
+	{ 
+		Console.WriteLine("escolha um caractere");
+		char numero = char.Parse(Console.ReadLine());
+		Console.Write("o caractere digitado  é " +numero);
+	Console.Write(Console.ReadLine());
+	
+	
+	
+	
+	Console.WriteLine("aperte qualquer tecla para fechar");
+		Console.ReadKey();
+    
+		
+	}
+}
